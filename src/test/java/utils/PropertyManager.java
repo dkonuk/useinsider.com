f428f823451835c12837aafe647a20dd3bf3a4bc
dkonuk <dkonuk@gmail.com>
2024-10-01 00:15:15 +0300

    Packages added

--- src/test/java/utils/PropertyManager.java
+++ src/test/java/utils/PropertyManager.java
@@ -1,4 +1,30 @@
 package utils;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyManager {
+    static Properties property;
+
+    public PropertyManager() {
+        Properties props = new Properties();
+        try {
+            props.load(new FileReader(new File(ClassLoader.getSystemResource("config.properties").getPath())));
+            property = props;
+        } catch (IOException e) {
+            // Handle Error
+            System.out.println("Properties file not found!");
+        }
+    }
+
+    public static String getProperty(String key) {
+        Properties props = property;
+        if (props != null) {
+            return props.getProperty(key);
+        }
+        return null;
+
+    }
 }
