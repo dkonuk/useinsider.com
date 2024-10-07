dc7391e95eb83e784eb8fa6071f550b7af0ab7f1
dkonuk <dkonuk@gmail.com>
2024-10-08 00:48:30 +0300

    NavBarSolutions Industries test added

--- src/test/java/pages/NavBarSolutionPage.java
+++ src/test/java/pages/NavBarSolutionPage.java
@@ -10,11 +10,15 @@ public class NavBarSolutionPage extends BasePage{
     public SelenideElement optimizeCustomerAcquisition = $(".new-menu-dropdown-layout-3-left-container > a.dropdown-sub:nth-child(2)");
     public SelenideElement increaseCustomerEngagement = $(".new-menu-dropdown-layout-3-left-container > a.dropdown-sub:nth-child(4)");
     public SelenideElement reduceCustomerChurn = $(".new-menu-dropdown-layout-3-left-container > a.dropdown-sub:nth-child(6)");
-
-
+    public String midContainer = ".new-menu-dropdown-layout-3-mid-container";
+    public String childSelector = "a.dropdown-sub-sub";
+    public int nthChild;
 
 
     public NavBarSolutionPage(String pageUrl) {
         super(pageUrl);
+    }
+    public SelenideElement getWebElement() {
+        return $("div.new-menu-dropdown-layout-3-mid-container > a.dropdown-sub-sub:nth-child(" + nthChild + ")");
     }
 }
--- src/test/java/tests/NavBarSolutionsPageTest.java
+++ src/test/java/tests/NavBarSolutionsPageTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.interactions.Actions;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static com.codeborne.selenide.WebDriverRunner.url;
 
 public class NavBarSolutionsPageTest extends BaseTest {
 
@@ -19,11 +20,15 @@ public class NavBarSolutionsPageTest extends BaseTest {
     public void TestNavBarSolutionsPage(){
         Actions actions = Selenide.actions();
         actions.click(pageManager.navBarSolutionPage.navBarSolution).click(pageManager.navBarSolutionPage.optimizeCustomerAcquisition).perform();
+        assertThat(url()).contains("onboarding");
         actions.click(pageManager.navBarSolutionPage.navBarSolution).click(pageManager.navBarSolutionPage.increaseCustomerEngagement).perform();
+        assertThat(url()).contains("engagement");
         actions.click(pageManager.navBarSolutionPage.navBarSolution).click(pageManager.navBarSolutionPage.reduceCustomerChurn).perform();
-//        assertThat(pageManager.navBarSolutionPage.optimizeCustomerAcquisition).hasText("Optimize Customer Acquisition");
-//        assertThat(pageManager.navBarSolutionPage.increaseCustomerEngagement).hasText("Increase Customer Engagement");
-//        assertThat(pageManager.navBarSolutionPage.reduceCustomerChurn).hasText("Reduce Customer Churn");
+        assertThat(url()).contains("churn");
+        int childSize = iterateOverAllChildren(pageManager.navBarSolutionPage.midContainer, pageManager.navBarSolutionPage.childSelector);
+        for (pageManager.navBarSolutionPage.nthChild = 2; pageManager.navBarSolutionPage.nthChild <= childSize; pageManager.navBarSolutionPage.nthChild++) {
+            actions.click(pageManager.navBarSolutionPage.navBarSolution).click(pageManager.navBarSolutionPage.getWebElement()).perform();
+        }
 
     }
 }
