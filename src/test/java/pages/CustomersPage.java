3a91fc668756163f8ef46156a04871f8543c410a
dkonuk <dkonuk@gmail.com>
2024-10-09 21:32:09 +0300

    CustomersPage added

--- src/test/java/pages/CustomersPage.java
+++ src/test/java/pages/CustomersPage.java
@@ -17,8 +17,31 @@ public class CustomersPage extends BasePage {
     public SelenideElement toyotaCaseStudy = $("#brand-details-1");
     public SelenideElement vodafoneCaseStudy = $("#brand-details-2");
     public SelenideElement adidasCaseStudy = $("#brand-details-3");
+    public SelenideElement nakdSelection = $x("//div[contains(text(),'increased lifetime value')]");
+    public SelenideElement madeiraSelection = $x("//div[contains(text(),'return of Investments (ROI)')]");
+    public SelenideElement belgianSelection = $x("//div[contains(text(),'faster campaign execution')]");
+    public SelenideElement nakdCaseStudy = $x("//*[contains(text(), 'achieved ROI')]");
+    public SelenideElement madeiraCaseStudy = $x("//*[contains(text(), 'Madeira')]");
+    public SelenideElement belgianCaseStudy = $x("//*[contains(text(), 'Belgian')]");
+    public String industrySliderParent = "div.industry-industry-nav:nth-child(2)";
+    public String industrySliderchildren = "button.industry-industry-button";
+    public int nthChild;
+    public String businessObjectiveSlider = "div.objective-objective-nav:nth-child(2)";
+    public String businessObjectiveChildren = " button.objective-objective-button";
+    public String businessObjectiveButtonsParent = "button.objective_visible_on_mobile.objective-slider-next:nth-child(3)";
+    public String businessObjectiveButtonsChildren = "div.objective-slider-item";
+    public SelenideElement businessObjectiveNextButton = $(".objective-slider-navigation:nth-child(10) > button.objective-slider-next");
+    public SelenideElement businessObjectivePreviousButton = $(".objective-slider-navigation:nth-child(10) > button.objective-slider-previous");
+
+
 
     public CustomersPage(String pageUrl) {
         super(pageUrl);
+    }
+    public SelenideElement getIndustrySliderWebElement() {
+        return $("div.industry-industry-nav:nth-child(2) > button.industry-industry-button:nth-child(" + nthChild + ")");
+    }
+    public SelenideElement getBusinessObjectiveSliderWebElement() {
+        return $("div.objective-objective-nav:nth-child(2) > button.objective-objective-button:nth-child(" + nthChild + ")");
     }
 }
--- src/test/java/tests/CustomersPageTest.java
+++ src/test/java/tests/CustomersPageTest.java
@@ -1,18 +1,60 @@
 package tests;
 
+import com.codeborne.selenide.Selenide;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.openqa.selenium.interactions.Actions;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CustomersPageTest extends BaseTest{
+public class CustomersPageTest extends BaseTest {
     @BeforeMethod
     public void before() {
         pageManager.customersPage.open();
         pageManager.homePage.acceptCookies.click();
     }
+
     @Test
-    public void TestCustomersPage(){
+    public void TestCustomersPage() {
         assertThat(pageManager.customersPage.customers.isDisplayed()).isTrue();
+        assertThat(pageManager.customersPage.slidingCustomerBanner.isDisplayed()).isTrue();
+        pageManager.customersPage.enterpriseButton.click();
+        pageManager.customersPage.toyotaSelection.click();
+        assertThat(pageManager.customersPage.toyotaCaseStudy.isDisplayed()).isTrue();
+        pageManager.customersPage.vodafoneSelection.click();
+        assertThat(pageManager.customersPage.vodafoneCaseStudy.isDisplayed()).isTrue();
+        pageManager.customersPage.adidasSelection.click();
+        assertThat(pageManager.customersPage.adidasCaseStudy.isDisplayed()).isTrue();
+        pageManager.customersPage.highgrowthButton.click();
+        pageManager.customersPage.nakdSelection.click();
+        assertThat(pageManager.customersPage.nakdCaseStudy.isDisplayed()).isTrue();
+        pageManager.customersPage.madeiraSelection.click();
+        assertThat(pageManager.customersPage.madeiraCaseStudy.isDisplayed()).isTrue();
+        pageManager.customersPage.belgianSelection.click();
+        assertThat(pageManager.customersPage.belgianCaseStudy.isDisplayed()).isTrue();
+        int childSize = iterateOverAllChildren(pageManager.customersPage.industrySliderParent, pageManager.customersPage.industrySliderchildren);
+        for (pageManager.customersPage.nthChild = 1; pageManager.customersPage.nthChild <= childSize; pageManager.customersPage.nthChild++) {
+            Actions actions = Selenide.actions();
+            actions.click(pageManager.customersPage.getIndustrySliderWebElement()).perform();
+        }
+        int businessObjectiveSliderChildSize = iterateOverAllChildren(pageManager.customersPage.businessObjectiveSlider, pageManager.customersPage.businessObjectiveChildren);
+        for (pageManager.customersPage.nthChild = 1; pageManager.customersPage.nthChild <= businessObjectiveSliderChildSize; pageManager.customersPage.nthChild++) {
+            Actions actions = Selenide.actions();
+            actions.click(pageManager.customersPage.getBusinessObjectiveSliderWebElement()).perform();
+        }
+        int businessObjectiveButtonChildSize = iterateOverAllChildren(pageManager.customersPage.businessObjectiveButtonsParent, pageManager.customersPage.businessObjectiveButtonsChildren);
+        System.out.println(businessObjectiveButtonChildSize);
+        for (pageManager.customersPage.nthChild = 1; pageManager.customersPage.nthChild <= businessObjectiveButtonChildSize; pageManager.customersPage.nthChild++) {
+            pageManager.customersPage.businessObjectiveNextButton.click();
+            Actions actions = Selenide.actions();
+        actions.pause(5000).perform();
+        }
+        for (pageManager.customersPage.nthChild = 1; pageManager.customersPage.nthChild <= businessObjectiveButtonChildSize; pageManager.customersPage.nthChild++) {
+            pageManager.customersPage.businessObjectivePreviousButton.click();
+            Actions actions = Selenide.actions();
+        actions.pause(5000).perform();
+        }
+
     }
 }
