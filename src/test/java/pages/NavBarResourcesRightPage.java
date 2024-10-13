b0ef7483c8b360b09fce62790045bc06960e1409
dkonuk <dkonuk@gmail.com>
2024-10-13 16:50:25 +0300

    NavbarSourcesRightPage and test added

--- src/test/java/pages/NavBarResourcesRightPage.java
+++ src/test/java/pages/NavBarResourcesRightPage.java
@@ -6,14 +6,14 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class NavBarResourcesRightPage extends BasePage {
-    public String midContainer = ".new-menu-dropdown-layout-5-mid-container";
-    public String childSelector = "a.dropdown-sub-sub";
+    public String navBarResourcesRight = ".new-menu-dropdown-layout-5-mid-container";
+    public String navBarResourcesRightChildren = "a.dropdown-sub-sub";
     public int nthChild;
 
     public NavBarResourcesRightPage(String pageUrl){
         super(pageUrl);
     }
     public SelenideElement getWebElement() {
-        return $("div.new-menu-dropdown-layout-2-mid-container > a.dropdown-sub-sub:nth-child(" + nthChild + ")");
+        return $("div.new-menu-dropdown-layout-5-mid-container > a.dropdown-sub-sub:nth-child(" + nthChild + ")");
     }
 }
--- src/test/java/tests/NavBarResourcesRightPageTest.java
+++ src/test/java/tests/NavBarResourcesRightPageTest.java
@@ -1,4 +1,23 @@
 package tests;
 
-public class NavBarResourcesRightPageTest {
+import com.codeborne.selenide.Selenide;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.interactions.Actions;
+
+public class NavBarResourcesRightPageTest extends BaseTest {
+    @BeforeMethod
+    public void before() {
+        pageManager.navBarResourcesRightPage.open();
+        pageManager.homePage.acceptCookies.click();
+    }
+
+    @Test
+    public void TestNavBarResourcesRightPage() {
+        int childSize = iterateOverAllChildren(pageManager.navBarResourcesRightPage.navBarResourcesRight, pageManager.navBarResourcesRightPage.navBarResourcesRightChildren);
+        for (pageManager.navBarResourcesRightPage.nthChild = 2; pageManager.navBarResourcesRightPage.nthChild <= childSize; pageManager.navBarResourcesRightPage.nthChild++) {
+            Actions actions = Selenide.actions();
+            actions.click(pageManager.navBarResourcesPage.navBarResources).click(pageManager.navBarResourcesRightPage.getWebElement()).perform();
+        }
+    }
 }
